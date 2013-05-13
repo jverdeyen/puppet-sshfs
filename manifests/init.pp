db84484eb7fe7bdb9d220f2d9a84bc3c95a4ad3c
Joeri Verdeyen <info@jverdeyen.be>
2013-05-13 10:17:56 +0200

    Changed sshfs path

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@
 class sshfs {
 
   package { 'OSXFuse sshfs':
-    source   => 'https://github.com/downloads/osxfuse/sshfs/SSHFS-2.4.1.pkg',
+    source   => 'http://cloud.github.com/downloads/osxfuse/sshfs/SSHFS-2.4.1.pkg',
     provider => 'apple'
     # require  => Package['Growl Fork']
   }
